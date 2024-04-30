f8ef0cf604995010b0a6b316895e3d3c586585da
hemreduru <hemreduru@gmail.com>
2024-04-30 23:34:28 +0300

    fixed bugs

--- game.js
+++ game.js
@@ -8,48 +8,55 @@ let guessCount = 3;
 
 function theGame() {
     var num = parseInt(document.getElementById("guessInput").value);
-    if (guessCount == 1) {
-        document.getElementById("tahmin").innerText = guessCount--;
-        disableInput();
-        document.getElementById("message").innerText = "Tahmin hakkınız kalmadı. Sayı: " + correctNumber + "'idi.";
-        const buton = document.createElement("button");
-        buton.textContent = "Yeniden Oyna";
-        buton.classList.add("btn", "btn-outline-info");
-        document.getElementById("alert").insertAdjacentElement("afterend", buton);
-        buton.addEventListener("click", function () {
-            window.location.reload();
-        });
+    if (!num) {
+        document.getElementById("message").innerText = "Boş bırakmayınız.";
 
     }
     else {
-        if (num < 1 || num > 10) {
-            document.getElementById("message").innerText = "1-10 arası bir sayi giriniz.";
-        }
-        else if (num < correctNumber) {
-            document.getElementById("message").innerText = "Yukarı";
-            guessCount--;
-            document.getElementById("tahmin").innerText = guessCount;
-        }
-        else if (num > correctNumber) {
-            document.getElementById("message").innerText = "Aşağı";
-            guessCount--;
-            document.getElementById("tahmin").innerText = guessCount;
-        }
-        else {
-            document.getElementById("message").innerText = "Tebrikler doğru sayı. Yeniden oynamak için butona bas. ";
+        if (guessCount == 1) {
+            document.getElementById("tahmin").innerText = guessCount--;
             disableInput();
+            document.getElementById("message").innerText = "Tahmin hakkınız kalmadı. Sayı: " + correctNumber + "'idi.";
             const buton = document.createElement("button");
             buton.textContent = "Yeniden Oyna";
             buton.classList.add("btn", "btn-outline-info");
-            document.getElementById("alert").insertAdjacentElement("afterend", buton);
+            document.getElementById("alert").appendChild(buton);
             buton.addEventListener("click", function () {
                 window.location.reload();
             });
+
+        }
+        else {
+            if (num < 1 || num > 10) {
+                document.getElementById("message").innerText = "1-10 arası bir sayi giriniz.";
+            }
+            else if (num < correctNumber) {
+                document.getElementById("message").innerText = "Yukarı";
+                guessCount--;
+                document.getElementById("tahmin").innerText = guessCount;
+            }
+            else if (num > correctNumber) {
+                document.getElementById("message").innerText = "Aşağı";
+                guessCount--;
+                document.getElementById("tahmin").innerText = guessCount;
+            }
+            else {
+                document.getElementById("message").innerText = "Tebrikler doğru sayı. Yeniden oynamak için butona bas. ";
+                disableInput();
+                const buton = document.createElement("button");
+                buton.textContent = "Yeniden Oyna";
+                buton.classList.add("btn", "btn-outline-info");
+                document.getElementById("alert").insertAdjacentElement("afterend", buton);
+                buton.addEventListener("click", function () {
+                    window.location.reload();
+                });
+            }
         }
     }
 }
 
 function disableInput() {
     document.getElementById('guessInput').disabled = true;
+    document.getElementById('button').disabled = true;
 }
 
